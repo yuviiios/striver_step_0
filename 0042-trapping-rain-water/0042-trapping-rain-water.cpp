b68605e1df6b283ce43db01a9d6e42ae02e65f4e
yuviiios <138116107+yuviiios@users.noreply.github.com>
2026-01-08 23:09:58 +0530

    Time: 0 ms (100%), Space: 26.1 MB (58.13%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -2,28 +2,26 @@ class Solution {
 public:
     int trap(vector<int>& height) {
         int n=height.size();
-        vector<int> lmax(n,0);
-        vector<int> rmax(n,0);
-        lmax[0]=height[0];
-        rmax[n-1]=height[n-1];
-
-        for(int i=1;i<n;i++) {
-            lmax[i]=max(lmax[i-1],height[i]);
-        }
-        for(int j=n-2;j>=0;j--) {
-            rmax[j]=max(rmax[j+1],height[j]);
-
-        }
-
+        int l=0,r=n-1;
+        int lmax=0,rmax=0;
         int ans=0;
-        for(int i=0;i<n;i++) {
-            ans+=min(lmax[i],rmax[i])-height[i];
+
+        while(l<r) {
+            lmax=max(lmax,height[l]);
+            rmax=max(rmax,height[r]);
+
+            if(lmax<rmax) {
+                ans+=lmax-height[l];
+                l++;
+            }
+            else {
+                ans+=rmax-height[r];
+                r--;
+            }
         }
 
         return ans;
-
-
-        
+       
 
         
     }
