b2e2318641143143de04075689ddbc65fecad051
yuviiios <138116107+yuviiios@users.noreply.github.com>
2026-02-15 23:57:46 +0530

    Time: 0 ms (100%), Space: 8.6 MB (13.32%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -3,7 +3,7 @@ public:
     int mySqrt(int x) {
         int l=1,h=x;
         while(l<=h) {
-            long long mid=l+(h-l)/2;
+            long long mid=l+(h-l)/2; //in the case of overflow always take mid=l+(h-l)/2
             long long value=(mid*mid);
             if(value<=x) {
                 l=mid+1;
