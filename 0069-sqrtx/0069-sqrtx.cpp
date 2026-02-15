973944530a2875eb5714ceea0d66c5f2f4766de9
yuviiios <138116107+yuviiios@users.noreply.github.com>
2026-02-15 23:57:16 +0530

    Time: 0 ms (100%), Space: 8.7 MB (13.32%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        int l=1,h=x;
+        while(l<=h) {
+            long long mid=l+(h-l)/2;
+            long long value=(mid*mid);
+            if(value<=x) {
+                l=mid+1;
+            }
+            else {
+                h=mid-1;
+            }
+
+        }
+        return h;
+        
+    }
+};
