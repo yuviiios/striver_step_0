e44ea888d781aa1e554f3eff08e6f07abf61681a
yuviiios <138116107+yuviiios@users.noreply.github.com>
2026-06-17 10:23:32 +0530

    Time: 0 ms (100%), Space: 10.5 MB (46.04%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        int n=s.length();
+        reverse(s.begin(),s.end());
+        string ans="";
+        for(int i=0;i<n;i++) {
+            string word="";
+            while( i<n && s[i]!=' ') {
+                word+=s[i];
+                i++;
+            }
+
+            reverse(word.begin(),word.end());
+
+            if(word.length()>0) {
+                ans+=" "+word;
+            }
+
+        }
+
+        return ans.substr(1);
+        
+    }
+};
