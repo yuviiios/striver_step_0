6b2501b396020fac72892a626c0fec4162e39845
yuviiios <138116107+yuviiios@users.noreply.github.com>
2026-06-21 23:49:21 +0530

    Time: 0 ms (100%), Space: 8.3 MB (35.22%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -5,6 +5,40 @@ public:
 
         return(s+s).find(goal)!=string::npos;
 
+
+
+
+
+
+
+
+
+//NOTE: 
+
+        //find() searches for a substring (or character) inside a string.
+        //It returns the starting index where the substring is found.
+        //string::npos is a special constant provided by the C++ string class.
+//         Why do we need it?
+
+// find() normally returns an index:
+
+// string s = "hello";
+
+// cout << s.find("ll");   // 2
+// cout << s.find("o");    // 4
+
+// But what should it return if the substring doesn't exist?
+
+// s.find("xyz");
+
+// C++ returns:
+
+// string::npos
+
+// which means:
+
+// "I searched the whole string and couldn't find it."
+
         
     }
 };
